9ea3e1f5184c111c717615a1744607692246cbb0
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-07 22:12:08 -0500

    GIniciando servicios

---
+++ mysite/taskapi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=  200,blank=False, null=False)
+    description = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False,blank=False, null=False)
+
+    def __str__(self):
+        return self.title
